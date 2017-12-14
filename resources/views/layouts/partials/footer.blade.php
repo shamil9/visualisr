9fff38fdcbcdf7a171e0110a3c8f49b4b24aaa11
shamil9 <shamil9@gmail.com>
2017-12-14 11:32:58 +0100

    Removed irrelevant links

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -8,12 +8,6 @@
                 <a href="{{ route('contact.create') }}">Contact</a>
             </div>
             <div class="level-item">
-                <a href="{{ route('faq') }}">FAQ</a>
-            </div>
-            <div class="level-item">
-                <a href="{{ route('tos') }}">Terms of service</a>
-            </div>
-            <div class="level-item">
                 <a href="https://shamil.be" class="made-by">
                     <img src="{{ asset('assets/img/as.svg') }}" alt="Shamil Altamirov">
                 </a>
